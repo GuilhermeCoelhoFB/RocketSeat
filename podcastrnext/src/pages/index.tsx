c6fec169f6d6fb9571edccc619ad1832bc4b7d27
Guilherme Fernandes <guilherme.coelhofb@gmail.com>
2021-04-21 08:26:53 -0300

    Home Page
    Almost Done the home of podcstr using next.js

---
+++ podcastrnext/src/pages/index.tsx
@@ -0,0 +1,28 @@
+// SPA
+// SSR
+// SSG
+import Head from 'next/head'
+import { Header } from "../components/Header";
+
+
+export default function Home(props) {
+  console.log(props.episodes)
+  return (
+    <div>
+      <h1>Index</h1>
+    </div>
+  )
+
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
